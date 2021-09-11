1b0e0b060d9c3225183d1fa2c43cc8c9d9f80a68
mategam <gameromateo@knights.ucf.edu>
2021-09-11 16:07:20 -0400

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Gamero Mateo
+ */
+import java.io.Console;
+public class App
+{
+    public static void main (String[] args)
+    {
+        Console con = System.console();
+        String first = con.readLine("What is the first number? ");
+        String second = con.readLine("What is the second number? ");
+        int num1 = Integer.parseInt(first);
+        int num2 = Integer.parseInt(second);
+        int sum = num1 + num2;
+        int dif = num1 - num2;
+        int prod = num1 * num2;
+        int quot = num1 / num2;
+        System.out.printf("%d + %d = %d\n%d - %d = %d\n%d * %d = %d\n%d / %d = %d\n ",
+                num1, num2, sum,  num1, num2, dif , num1, num2, prod, num1, num2, quot);
+    }
+}
